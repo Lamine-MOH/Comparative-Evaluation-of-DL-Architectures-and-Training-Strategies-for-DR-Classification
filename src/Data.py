009fc91cffdd129f83615b2b89f3150739d0e4cd
Mohammed Lamine <mmmbbbhhh17@gmail.com>
2026-06-25 10:44:21 +0100

    fix double binary label type

--- src/Data.py
+++ src/Data.py
@@ -180,7 +180,7 @@ def data_load(dataset_path, img_size, labels_type="Grading"):
 
     # Apply Labels Type
     y = torch.where(y == 0, 0, 1) if labels_type == "Binary" else\
-        torch.where(y == 0, 0, torch.where((y > 0) & (y < 4), 1, 2)) if labels_type == "Binary" else\
+        torch.where(y == 0, 0, torch.where((y > 0) & (y < 4), 1, 2)) if labels_type == "Grading" else\
         y
 
     # Define transformations
